f0be2a23571cf4baa34db86f4555840f354f13c3
ShubhamRDurugkar <35094667+ShubhamRDurugkar@users.noreply.github.com>
2018-03-31 15:00:57 +0530

    Login done successfully.

---
+++ WebContent/C_User/UserController.js
@@ -0,0 +1,39 @@
+/**
+ * UserController for login 
+ */
+myApp.controller("UserController",function($scope,$http,$location,$rootScope,$cookieStore)
+{
+	$scope.user={loginname:'',password:'',role:'',username:'',emailId:'',mobileNo:'',address:'',isOnline:''};
+	
+	$rootScope.login=function()
+	{
+		console.log("Logging Function");
+		
+		$http.post('http://localhost:8083/SChatMiddleWare/login',$scope.user)
+			.then(function(response)
+			{
+				console.log(response.status);
+				$scope.user=response.data;
+				$rootScope.currentUser=response.data;
+				$cookieStore.put('userDetails',response.data);
+				console.log($rootScope.currentUser.role);
+					if($rootScope.currentUser.role=="ROLE_ADMIN")
+					{
+						console.log('AdminPage');
+					}
+					else
+					{
+						console.log('UserPage');
+					}
+				$location.path("/");
+			});
+	};
+	$rootScope.logout=function()
+	{
+		console.log('Logout Function');
+		delete $rootScope.currentUser;
+		$cookieStore.remove('userDetails');
+		$location.path("/");
+	}
+	
+});
--- WebContent/js/MyRouteConfig.js
+++ WebContent/js/MyRouteConfig.js
@@ -1,26 +1,32 @@
 /**
  * myApp to navigate between all pages (SPA)
  */
-var myApp = angular.module("myApp", ["ngRoute"]);
+var myApp = angular.module("myApp", [ 'ngRoute',,'ngCookies']);
 myApp.config(function($routeProvider) {
 	console.log('I am in routeProvider');
-    $routeProvider
-    .when("/", {
-        templateUrl : "template/home.html"
-    })
-    .when("/Login", {
-        templateUrl : "template/Login.html"
-    })
-    .when("/Register", {
-        templateUrl : "template/Register.html"
-    })
-    .when("/ContactUs", {
-        templateUrl : "template/ContactUs.html"
-    })
-    .when("/AboutUs", {
-        templateUrl : "template/AboutUs.html"
-    })
-    .when("/Blog", {
-        templateUrl : "C_Blog/Blog.html"
-    });
+	$routeProvider.when("/", {
+		templateUrl : "template/home.html"
+	}).when("/Login", {
+		templateUrl : "C_User/Login.html"
+	}).when("/Register", {
+		templateUrl : "template/Register.html"
+	}).when("/ContactUs", {
+		templateUrl : "template/ContactUs.html"
+	}).when("/AboutUs", {
+		templateUrl : "template/AboutUs.html"
+	}).when("/Blog", {
+		templateUrl : "C_Blog/Blog.html"
+	});
+});
+
+myApp.run(function($rootScope, $cookieStore) {
+	console.log('I am in run function');
+	console.log($rootScope.currentUser);
+
+	if ($rootScope.currentUser == undefined) {
+		$rootScope.currentUser = $cookieStore.get('userDetails');
+	} else {
+		console.log($rootScope.currentUser.username);
+		console.log($rootScope.currentUser.role);
+	}
 });
