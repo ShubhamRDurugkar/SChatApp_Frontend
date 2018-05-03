8c672121f55f2df495a22a7758ced224c12760f2
ShubhamRDurugkar <35094667+ShubhamRDurugkar@users.noreply.github.com>
2018-05-03 17:29:34 +0530

    CHat implementation done successfully and updated route js file.

--- WebContent/js/MyRouteConfig.js
+++ WebContent/js/MyRouteConfig.js
@@ -8,8 +8,16 @@ myApp.config(function($routeProvider) {
 		templateUrl : "home.html"
 	}).when("/Login", {
 		templateUrl : "C_User/Login.html"
-	}).when("/UpdateProfile", {
+	}).when("/UpdateProfilePicture", {
 		templateUrl : "C_User/UpdateProfile.html"
+	}).when("/UpdateProfileDetails", {
+		templateUrl : "C_User/UpdateProfileDetails.html"
+	}).when("/ViewProfile", {
+		templateUrl : "C_User/ViewProfile.html"
+	}).when("/userRequests", {
+		templateUrl : "C_User/userRequests.html"
+	}).when("/suggestedFriends", {
+		templateUrl : "C_friend/suggestedFriends.html"
 	}).when("/Register", {
 		templateUrl : "C_User/Register.html"
 	}).when("/ContactUs", {
@@ -18,14 +26,24 @@ myApp.config(function($routeProvider) {
 		templateUrl : "template/AboutUs.html"
 	}).when("/Blog", {
 		templateUrl : "C_Blog/Blog.html"
+	}).when("/ViewBlog", {
+		templateUrl : "C_Blog/viewBlog.html"
+	}).when("/SingleBlog", {
+		templateUrl : "C_Blog/SingleBlog.html"
 	}).when("/updateBlog", {
 		templateUrl : "C_Blog/updateBlog.html"
 	}).when("/createForum", {
 		templateUrl : "C_forum/Forum.html"
 	}).when("/updateForum", {
 		templateUrl : "C_forum/updateForum.html"
+	}).when("/ViewForums", {
+		templateUrl : "C_forum/ViewForums.html"
+	}).when("/SingleForum", {
+		templateUrl : "C_forum/SingleForum.html"
 	}).when("/Chat", {
-	templateUrl : "C_chat/chat.html"
+		templateUrl : "C_chat/chat.html"
+	}).when("/Friends", {
+		templateUrl : "C_friend/showAllFriends.html"
 	});
 });
 
