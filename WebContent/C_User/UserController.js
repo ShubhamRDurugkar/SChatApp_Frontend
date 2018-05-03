2507d51277fe1b79f8dee1731666d64b9e9b464f
ShubhamRDurugkar <35094667+ShubhamRDurugkar@users.noreply.github.com>
2018-05-03 17:25:18 +0530

    Logout function with status change done.

--- WebContent/C_User/UserController.js
+++ WebContent/C_User/UserController.js
@@ -13,6 +13,8 @@ myApp.controller("UserController", function($scope, $http, $location,
 		address : '',
 		isOnline : ''
 	};
+	$scope.userData;
+	$scope.userList;
 	$scope.userProfile = {
 		loginname : '',
 		image : ''
@@ -46,16 +48,58 @@ myApp.controller("UserController", function($scope, $http, $location,
 		});
 
 	};
+	$scope.update = function(loginname) {
+		console.log('Entered into the Update  User method');
+		$http.post("http://localhost:8083/SChatMiddleWare/updateUser/"+loginname,
+				$scope.user).then(function(response) {
+			console.log('Status text:' + response.statusText);
+			alert('updated details successfully..!!');
+			$location.path("/ViewProfile");
+		});
 
+	};
+
+	/*
+	 * $scope.upload = function() { console.log('Entered into the upload
+	 * method');
+	 * $http.post("http://localhost:8083/SChatMiddleWare/doUpload",$scope.userProfile).then(function(response) {
+	 * console.log('Status text:' + response.statusText); alert('Uploaded
+	 * successfully..!!'); $location.path("/"); }); };
+	 */
+	$scope.fetchUserDetails = function() {
+		console.log("Inside fetch user Details function "
+				+ $rootScope.currentUser.loginname);
+		$http.get(
+				"http://localhost:8083/SChatMiddleWare/getUser/"
+						+ $rootScope.currentUser.loginname).then(
+				function(response) {
+					console.log("fetched User");
+					$scope.userData = response.data;
+					$scope.user = response.data;
+					$rootScope.currentUser = response.data;
+					$cookieStore.put('userDetails', response.data);
+				});
+	};
+	/*$scope.listUsers = function() {
+		console.log("Inside list user Details function "
+				+ $rootScope.currentUser.loginname);
+		$http.get("http://localhost:8083/SChatMiddleWare/listUsers").then(
+				function(response) {
+					console.log("fetched Users");
+					$scope.userList = response.data;
+					$scope.user = response.data;
+					$cookieStore.put('userDetails', response.data);
+				});
+	};*/
 	$rootScope.logout = function() {
 		console.log('Entered into the logout function');
-		$http.post("http://localhost:8083/SChatMiddleWare/logout", $rootScope.currentUser)
-				.then(function(response) {
-					console.log(response.data);
-					delete $rootScope.currentUser;
-					$cookieStore.remove('userDetails');
-					$location.path("/");
-				});
+		$http.post("http://localhost:8083/SChatMiddleWare/logout",
+				$rootScope.currentUser).then(function(response) {
+			console.log(response.data);
+			delete $rootScope.currentUser;
+			$cookieStore.remove('userDetails');
+			$location.path("/");
+		});
 	}
 
 });
